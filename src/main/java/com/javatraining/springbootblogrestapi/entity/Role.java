7d2c2f2d8993479629985b3eac4e9f53c452716a
satish <kacharlasatish546@gmail.com>
2023-12-24 14:24:00 +0530

    Added Entity for User, Role Added Repository for User and Role

---
+++ src/main/java/com/javatraining/springbootblogrestapi/entity/Role.java
@@ -0,0 +1,26 @@
+package com.javatraining.springbootblogrestapi.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+
+}
---
+++ src/main/java/com/javatraining/springbootblogrestapi/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.javatraining.springbootblogrestapi.repository;
+
+import com.javatraining.springbootblogrestapi.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+    
+}
